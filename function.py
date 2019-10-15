d63cb14691376ca5f7253708e54dace47836f59d
dadhichvivek <56474890+dadhichvivek@users.noreply.github.com>
2019-10-15 20:19:28 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,12 @@
+a=input("Enter a name ")
+i=0
+l=len(a)
+while(l>i):
+    l=l-1
+    if(a[i]!=a[l]):
+        print("Not a palindrome")
+        break
+    else:
+        i=i+1
+    print("Palindrome")
+    break
